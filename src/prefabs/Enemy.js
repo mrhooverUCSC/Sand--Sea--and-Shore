cd84f9a6a897e9897853b63dd6663f15673243a1
Ivan Martinez-Arias <50786762+UBSNASTy@users.noreply.github.com>
2021-05-12 20:47:32 -0700

    Added enemy class and spawned 1 in the Play scene

---
+++ src/prefabs/Enemy.js
@@ -0,0 +1,19 @@
+class Enemy extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, environment, speed) {
+        super(scene, x, y, texture);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.setVelocityX(speed);
+        this.body.onCollide = true;
+        this.body.setImmovable(true);
+
+        this.environment = environment;
+        console.log(`environment: ${environment} / speed: ${speed}`);
+    }
+
+    update() {
+        if(this.x < -this.width) {
+            this.destroy();
+        }
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,7 @@ class Play extends Phaser.Scene {
 
         this.load.image('player', 'images/PlayerBall.png');
         this.load.image('shot', 'images/PlayerShot.png');
+        this.load.image('crab', 'images/newCrab.jpg');
     }
 
     create(){
@@ -19,10 +20,19 @@ class Play extends Phaser.Scene {
 
         this.player = new Player(this, game.config.width / 3, game.config.height / 3, 'player').setOrigin(0.5, 0.5);
 
+        this.enemyGroup = this.add.group({
+            runChildUpdate: true
+        })
+
+        // adds new enemy when starting after 3 seconds
+        this.time.delayedCall(3000, () => {
+            this.addEnemy();
+        })
     }
 
     update() {
         this.player.update();
+
         if(keyRIGHT.isDown && playerShotAvailable){
             new PlayerShot(this, this.player.x + this.player.width/2 * Math.cos(this.player.rotation), this.player.y + this.player.height/2 * Math.sin(this.player.rotation), 'shot', this.player.rotation).setOrigin(0.5, 0.5);
             playerShotAvailable = false;
@@ -34,5 +44,17 @@ class Play extends Phaser.Scene {
                 loop: false
             })
         }
+        
+        this.physics.world.collide(this.player, this.enemyGroup, this.collisionOccurred, null, this);
+
+    }
+
+    addEnemy() {
+        let newEnemy = new Enemy(this, game.config.width, game.config.height - 200, 'crab', 'land', -100);
+        this.enemyGroup.add(newEnemy);
+    }
+
+    collisionOccurred() {
+        console.log("collision detected");
     }
 }
