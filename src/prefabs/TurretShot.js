503c38a7313320b3f12840d0ba30dca32b4aab3c
mrhooverUCSC <61717155+mrhooverUCSC@users.noreply.github.com>
2021-05-16 15:15:31 -0700

    Collision using "overlap" instead of "collide" for hit detection

--- src/prefabs/TurretShot.js
+++ src/prefabs/TurretShot.js
@@ -14,13 +14,6 @@ class TurretShot extends Phaser.Physics.Arcade.Sprite {
         this.setRotation(Phaser.Math.Angle.Between(this.x, this.y, this.target.x, this.target.y)); //aim at it
         this.setVelocity(Math.cos(this.rotation) * this.speedMultiplier, Math.sin(this.rotation) * this.speedMultiplier);
 
-        /*if (this.x < this.target.x + this.target.width && 
-            this.x + this.width > this.target.x && 
-            this.y < this.target.y + this.target.height &&
-            this.height + this.y > this.target.y) {
-            this.scene.enemyHitByPlayer(this.target, this);
-        }*/
-
         if(this.x < 0 || this.x > game.config.width || this.y < 0 || this.y > game.config.height){
             this.destroy();
         }
